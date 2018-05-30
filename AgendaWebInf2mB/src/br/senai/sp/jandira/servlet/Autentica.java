9e8ae1e3ad24c7ddc4663dadff51c039e07c3b83
sn1006137 <sn1006137@DSN1271058284.cfp127aluno.local>
2018-05-30 12:51:40 -0300

    Versão 30-05-2018 11:30

--- AgendaWebInf2mB/src/br/senai/sp/jandira/servlet/Autentica.java
+++ AgendaWebInf2mB/src/br/senai/sp/jandira/servlet/Autentica.java
@@ -1,6 +1,8 @@
-package br.senai.sp.servlet;
+package br.senai.sp.jandira.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,10 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/Autentica")
+@WebServlet("/Autentica") 
 public class Autentica extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
     public Autentica() {
         super();
@@ -19,11 +20,19 @@ public class Autentica extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("USANDO O MÉTODO GET!");
+		
+		response.getWriter().write("Usando o método GET!");
 	}
 
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		System.out.println("USANDO O MÉTODO POST!");
+	
+		request.setAttribute("frase", "Esta é uma frase qualquer!");
+		
+		RequestDispatcher rd = request.getRequestDispatcher("teste.jsp");
+		rd.forward(request, response);
+		
 	}
 
 }
